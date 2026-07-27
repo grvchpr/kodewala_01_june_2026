885293ba6d703e38aeeff51753e08cb67b45f641
Gourav Chopra <chopragourav1406@gmail.com>
2026-07-27 16:06:22 +0530

    add discount functionality in FunctionExample class

--- FunctionalInterface/src/function/FunctionExample.java
+++ FunctionalInterface/src/function/FunctionExample.java
@@ -6,5 +6,8 @@ public class FunctionExample {
 	public static void main(String[] args) {
 		Function<String, Integer> function = (s) -> s.length();
 		System.out.println(function.apply("Gourav")); // 6
+
+		Function<Integer, Integer> calculateDiscount = (i) -> i = i - (i * 10 / 100);
+		System.out.println("After discount final price: " + calculateDiscount.apply(100)); // 90
 	}
 }
